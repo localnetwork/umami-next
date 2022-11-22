4fa08250a7551110d08aa71548e2e892dd06f1c7
Dion <diome.halcyonwebdesign@gmail.com>
2022-11-22 14:05:02 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 const { i18n } = require("./next-i18next.config")
 
-module.exports = {
-  i18n,
-  images: {
-    domains: [process.env.NEXT_IMAGE_DOMAIN],
-  },
-}
-
 // module.exports = {
 //   i18n,
-//   reactStrictMode: true,
 //   images: {
-//     domains: ['staging.test-dion.halcyondigitalhost.com'],
+//     domains: [process.env.NEXT_IMAGE_DOMAIN],
 //   },
 // }
+
+module.exports = {
+  i18n,
+  // reactStrictMode: true,
+  images: {
+    domains: ['http:localhost:3000', 'staging.test-dion.halcyondigitalhost.com'],
+  },
+}
